fc07e50264dfd59f863ac5c1c7f6aa1e50974a73
AnacondaPython <thechinglisher@gmail.com>
2016-11-04 12:36:43 -0400

    Bad branch notes

--- pset4/resize.c
+++ pset4/resize.c
@@ -15,15 +15,24 @@
 int main(int argc, char* argv[])
 {
     // ensure proper usage
-    if (argc != 3)
+    // 4, because we need to add a factor
+    if (argc != 4)
     {
-        printf("Usage: ./copy infile outfile\n");
+        printf("Usage: ./copy factor infile outfile\n");
         return 1;
+    }
+    
+    //added this file for input name
+    int factor = atoi(argv[1]);
+    if (argv[1] <= 0)
+    {
+        printf("Must be positive factor\n");
+        return 6;
     }
 
     // remember filenames
-    char* infile = argv[1];
-    char* outfile = argv[2];
+    char* infile = argv[2]; //change to argv[2] for newer input
+    char* outfile = argv[3];
 
     // open input file 
     FILE* inptr = fopen(infile, "r");
@@ -43,37 +52,54 @@ int main(int argc, char* argv[])
     }
 
     // read infile's BITMAPFILEHEADER
-    BITMAPFILEHEADER bf;
-    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+    BITMAPFILEHEADER old_bf;
+    fread(&old_bf, sizeof(BITMAPFILEHEADER), 1, inptr);
 
     // read infile's BITMAPINFOHEADER
-    BITMAPINFOHEADER bi;
-    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+    BITMAPINFOHEADER old_bi;
+    fread(&old_bi, sizeof(BITMAPINFOHEADER), 1, inptr);
 
     // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
-    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 || 
-        bi.biBitCount != 24 || bi.biCompression != 0)
+    if (old_bf.bfType != 0x4d42 || old_bf.bfOffBits != 54 || old_bi.biSize != 40 || 
+        old_bi.biBitCount != 24 || old_bi.biCompression != 0)
     {
         fclose(outptr);
         fclose(inptr);
         fprintf(stderr, "Unsupported file format.\n");
         return 4;
     }
-
-    // write outfile's BITMAPFILEHEADER
-    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
-
-    // write outfile's BITMAPINFOHEADER
-    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
+    
+    
+    //Files been read for old_bf and old_bi. 
+    
+    //new BITMAP FILE HEADER CLASS to store the data;
+    BITMAPFILEHEADER new_bf; //14 bytes , same number of bytes but diff values
+    BITMAPINFOHEADER new_bi; //40 bytes, same # of bytes but diff values
+    
+    //opens new file to start rewriting new data. Now input and output files are open
+    fwrite(&new_bf, sizeof(BITMAPFILEHEADER),1, outptr);
+    fwrite(&new_bi, sizeof(BITMAPINFOHEADER),1 ,outptr);
+    
+    //Initialize new values inside the bitmap such as .biwidth, relative to input factor
+    new_bi.biWidth = old_bi.biWidth*factor;
+    new_bi.biHeight = old_bi.Height*factor;
+    
+    int old_padding = (4-)
+    
+    
+    //Initialize new values inside the bitmapfileheader
+    
+    // determine padding for scanlines
+    
 
     // determine padding for scanlines
-    int padding =  (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+    int old_padding =  (4 - (old_bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
 
     // iterate over infile's scanlines
-    for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
+    for (int i = 0, o_biHeight = abs(old_bi.biHeight); i < o_biHeight; i++)
     {
         // iterate over pixels in scanline
-        for (int j = 0; j < bi.biWidth; j++)
+        for (int j = 0; j < old_bi.biWidth; j++)
         {
             // temporary storage
             RGBTRIPLE triple;
